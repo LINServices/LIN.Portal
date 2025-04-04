6fbb5c4244b10abe949763c641702d036550cbe2
Alex Giraldo <giraldojhong4@gmail.com>
2025-04-03 20:07:47 -0500

    Mejoras y SEO

--- LIN.Portal/Program.cs
+++ LIN.Portal/Program.cs
@@ -1,23 +1,44 @@
 using LIN.Portal.Components;
+using Microsoft.AspNetCore.ResponseCompression;
+using System.IO.Compression;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Agregar compresión de respuesta
+builder.Services.AddResponseCompression(options =>
+{
+    options.EnableForHttps = true;
+    options.Providers.Add<BrotliCompressionProvider>();
+    options.Providers.Add<GzipCompressionProvider>();
+});
+
+builder.Services.Configure<BrotliCompressionProviderOptions>(options =>
+{
+    options.Level = CompressionLevel.Fastest;
+});
+
+builder.Services.Configure<GzipCompressionProviderOptions>(options =>
+{
+    options.Level = CompressionLevel.SmallestSize;
+});
+
+// Agregar servicios de Blazor
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configuración del pipeline HTTP
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 
+// Aplicar la compresión antes de servir los archivos estáticos y los componentes Blazor
+app.UseResponseCompression();
 
 app.UseAntiforgery();
 
---
+++ LIN.Portal/tailwind.config.js
@@ -0,0 +1,43 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["../**/*{html,razor,js,cs}"],
+    safelist: [
+        {
+            pattern: /bg-\w+-\d+/,
+        },
+        {
+            pattern: /text-\w+-\d+/,
+        },
+    ],
+    theme: {
+        screens: {
+            'sm': '640px',
+            'md': '768px',
+            'dl': '910px',
+            'lg': '1024px',
+            'xl': '1280px',
+            '2xl': '1536px',
+        },
+        extend: {
+            colors: {
+
+                light: "#F7F8FD",
+                'current': {
+                    '50': '#f5f7fa',
+                    '100': '#eaeef4',
+                    '200': '#d1dbe6',
+                    '300': '#a9bcd0',
+                    '400': '#7a98b6',
+                    '500': '#597b9c',
+                    '600': '#466383',
+                    '700': '#3a506a',
+                    '800': '#334559',
+                    '900': '#2e3c4c',
+                    '950': '#1e2733'
+                }
+            }
+
+        }
+    },
+    plugins: [],
+}
